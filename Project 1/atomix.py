a9828bfd02893bfdb364b1533dad889a0dc72006
201905451 <up201905451@up.pt>
2023-03-26 17:28:03 +0100

    confusion

--- Project 1/atomix.py
+++ Project 1/atomix.py
@@ -50,16 +50,46 @@ while is_running:
 def initialState(filename):
     boardgame = []
     solution = []
+    atoms = []
     f = open(filename,'r')
     line = f.readline().strip()
 
-    while line[0] == '#':
-        tmp = []
-        for i in range(1,len(line)):
-            tmp.append(i)
-        solution.append(tmp)
+    while line[0] == "#":
+        tmp = line.split()
+
+        a = atomState(0,0," ",0,0,0,0)
+        a.set_s(tmp[1])
+        a.set_x(tmp[2])
+        a.set_y(tmp[3])
+        
+        for i in range(0,len(tmp[4])):
+            if tmp[4][i] == "U" : a.set_c_u(a.c_u + 1)
+            elif tmp[4][i] == "D" : a.set_c_d(a.d_u + 1)
+            elif tmp[4][i] == "L" : a.set_c_l(a.l_u + 1)
+            elif tmp[4][i] == "R" : a.set_c_r(a.r_u + 1)
+
+        solution.append(a)
         line = f.readline().strip()
-    
+
+    line = f.readline().strip()
+
+    while line[0] == "#":
+        tmp = line.split()
+
+        a = atomState(0,0," ",0,0,0,0)
+        a.set_s(tmp[1])
+        a.set_x(tmp[2])
+        a.set_y(tmp[3])
+        
+        for i in range(0,len(tmp[4])):
+            if tmp[4][i] == "U" : a.set_c_u(a.c_u + 1)
+            elif tmp[4][i] == "D" : a.set_c_d(a.d_u + 1)
+            elif tmp[4][i] == "L" : a.set_c_l(a.l_u + 1)
+            elif tmp[4][i] == "R" : a.set_c_r(a.r_u + 1)
+
+        atoms.append(a)
+        line = f.readline().strip()
+
     line = f.readline().strip()
 
     while line != "":
@@ -69,9 +99,12 @@ def initialState(filename):
         boardgame.append(tmp)
         line = f.readline().strip()
     
-    return [solution, boardgame]
+    return [solution, boardgame, atoms]
 
-def printBoard(boardgame):
+def printBoard(boardgame, atoms):
+    for i in range(0,len(atoms)):
+        boardgame[atoms[i].y][atoms[i].x] = atoms[i].s
+    
     for i in boardgame:
         line = ""
         for j in i:
@@ -81,7 +114,20 @@ def printBoard(boardgame):
 class atomState:
     x = 0
     y = 0
-    s = ''
+    s = " "
+    c_u = 0
+    c_d = 0
+    c_l = 0
+    c_r = 0
+
+    def __init__(self,x1,y1,s1,c1,c2,c3,c4):
+        self.x = x1
+        self.y = y1
+        self.s = s1
+        self.c_u = c1
+        self.c_d = c2
+        self.c_l = c3
+        self.c_r = c4
 
     def set_x(x1):
         x = x1
@@ -91,7 +137,18 @@ class atomState:
 
     def set_s(s1):
         s = s1
-
+    
+    def set_c_u(c1):
+        c_u = c1
+    
+    def set_c_d(c1):
+        c_d = c1
+    
+    def set_c_l(c1):
+        c_l = c1
+    
+    def set_c_r(c1):
+        c_r = c1
 
 def move_up(atom,boardgame):
     x1 = atom.x
@@ -141,7 +198,7 @@ def move_left(atom,boardgame):
 
     return boardgame
 
-def move_down(atom,boardgame):
+def move_right(atom,boardgame):
     x1 = atom.x
     y1 = atom.y
 
@@ -156,4 +213,29 @@ def move_down(atom,boardgame):
     boardgame[atom.y][atom.x] = atom.s
 
     return boardgame
+
+def verify_solution(solution,boardgame):
+    d1 = 0
+    d2 = 0
+    for i in range(0,len(solution)):
+        for j in range(0,len(solution[i])):
+            if j == " ": continue
+            else:
+                for k in range(0,len(boardgame)):
+                    for l in range(0,len(boardgame[k])):
+                        if (l == " " | l == "X"): continue
+                        elif l != j : break
+                        else:
+                            d1 = l - j
+                            d2 = k - i
+
+    if d2 == 0: return 1
+    else:
+        for i in range(0,len(solution)):
+            for j in range(0, len(solution[i])):
+                if solution[i][j] == " " & (boardgame[i+d2][j+d1] == " " | boardgame[i+d2][j+d1] == "X"):continue
+                elif solution[i][j] == boardgame[i+d2][j+d1] : continue
+                else: return 1
+
+    return 0
 
